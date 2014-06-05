f680cefe18774b05eb12ddc95fa9d47f1f87ab12
Sharlikran <danielhmpdx16@gmail.com>
2014-06-05 07:46:57 -0700

    Added Temp file for possible location translation

---
+++ Localization/HarvesterMapStruct.lua
@@ -0,0 +1,29 @@
+Harvester.newMapdata = {
+    ["stonefalls"] = {
+        ["bleakrock_base"] = {
+            ["orkeyshollow"] = { "Orkey's Hollow" }
+            ["hozzinsfolly"] = { "Hozzin's Folly" }
+            ["companionspoint"] = { "Companions Point" }
+            ["bleakrockwayshrine"] = { "Bleakrock Wayshrine" }
+            ["hunterscamp"] = { "Hunter's Camp" }
+            ["frostedgecamp"] = { "Frostedge Camp" }
+            ["paddlefloefishingcamp"] = { "Paddlefloe Fishing Camp" }
+            ["bleakrockvillage"] = { "Bleakrock Village" }
+            ["skyshroudbarrow"] = { "Skyshroud Barrow" }
+            ["bleakrockisle"] = { "Bleakrock Isle" }
+        }
+        ["davonswatch_base"] = {
+            ["davonswatch"] = { "Davon's Watch" }
+        }
+        ["orkeyshollow_base"] = {
+            ["orkeyshollow"] = { "Orkey's Hollow" }
+        }
+        ["stonefalls_base"] = {
+            ["davonswatchwayshrine"] = { "Davon's Watch Wayshrine" }
+    }
+    ["bleakrock"] = {
+        ["bleakrockvillage_base"] = {
+            ["hunterscamp"] = { "Hunter's Camp" }
+        }
+    }
+}
